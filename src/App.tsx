1be698cd54ba752fc53ce9755cc5169a11527e21
tkachiko <tkachikos@gmail.com>
2022-09-11 00:08:17 +0300

    add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header";
-import {Technologies} from "./components/Technologies";
+import {Header} from './components/Header';
+import {Technologies} from './components/Technologies';
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Technologies/>
+      <Footer/>
     </div>
   );
 }
