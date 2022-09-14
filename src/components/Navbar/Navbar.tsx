98a281d8ac16c50532b8ba32c50bbf65ff12ea7a
tkachiko <tkachikos@gmail.com>
2022-09-15 00:33:34 +0300

    refactor styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ function Navbar() {
   return (
     <nav className={s.nav}>
       <ul>
-        <li><a href={'./settings'}>Profile</a></li>
-        <li><a href={'#'}>Messages</a></li>
-        <li><a href={'#'}>News</a></li>
-        <li><a href={'#'}>Music</a></li>
-        <li><a href={'#'}>Settings</a></li>
+        <li className={s.item}><a>Profile</a></li>
+        <li className={`${s.item} ${s.active}`}><a>Messages</a></li>
+        <li className={s.item}><a>News</a></li>
+        <li className={s.item}><a>Music</a></li>
+        <li className={s.item}><a>Settings</a></li>
       </ul>
     </nav>
   )
