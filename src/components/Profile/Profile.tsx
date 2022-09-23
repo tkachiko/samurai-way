c726fad1fe41802ffa2526af12ac4ecc15ee7237
tkachiko <tkachikos@gmail.com>
2022-09-23 23:43:37 +0300

    refactor Profile component

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './MyPosts/ProfileInfo/ProfileInfo';
 
 const Profile = () => {
   return (
     <div>
-      <div className={s.main}>
-        <img
-          src={'https://img.freepik.com/free-vector/night-ocean-landscape-full-moon-and-stars-shine_107791-7397.jpg?w=2000'}
-          alt={'cover'}/>
-        <div>
-          <img src={'https://thumbs.dreamstime.com/b/frenchbulldog-198912643.jpg'} alt={'user avatar'}/> + description
-        </div>
+      <div>
+        <ProfileInfo/>
         <MyPosts/>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Profile;
