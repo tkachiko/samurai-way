d4733129b67cf3dbb68053617f16a08d8220cf00
tkachiko <tkachikos@gmail.com>
2022-09-28 00:15:15 +0300

    add props, refactor routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,23 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Dialogs from './components/Dialogs/Dialogs';
+import {DialogsDataType, MessagesDataType, PostsDataType} from './index';
 
-const App = () => {
+type AppType = {
+  dialogs: DialogsDataType[]
+  messages: MessagesDataType[]
+  posts: PostsDataType[]
+}
+
+const App = (props: AppType) => {
   return (
     <BrowserRouter>
       <div className={'appWrapper'}>
         <Header/>
         <Navbar/>
         <div className="app-wrapper-content">
-          <Route path={'/dialogs'} component={Dialogs} />
-          <Route path={'/profile'} component={Profile} />
+          <Route path={'/dialogs'} render={() => <Dialogs dialogs={props.dialogs} messages={props.messages}/>} />
+          <Route path={'/profile'} render={() => <Profile posts={props.posts}/>} />
         </div>
         <Footer/>
       </div>
