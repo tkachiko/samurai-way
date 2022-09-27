69f0477c220c2a0d98d82d2a156bcff66425294d
tkachiko <tkachikos@gmail.com>
2022-09-27 23:36:05 +0300

    refactor styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,12 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      it-incubator.com
+      <div className={styles.content}>
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github icon"/>
+        <a href={'https://github.com/tkachiko'} target={'_blank'} rel={'noreferrer'}> tkachiko </a>2022
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
