5124a54b7494267f379836f9dce58fed92815094
tkachiko <tkachikos@gmail.com>
2022-10-03 21:59:23 +0300

    onClick, ref, VirtualDOM

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -13,10 +13,24 @@ export const Dialogs: React.FC<DialogsType> = (props) => {
   const dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} key={d.id}/>);
   const messagesElements = props.messages.map(m => <Message message={m.message} id={m.id} key={m.id}/>);
 
+  let newMessageElement = React.createRef<HTMLTextAreaElement>();
+
+  const sendMessage = () => {
+    let text = newMessageElement.current?.value;
+    alert(text);
+  };
+
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogsItems}>{dialogsElements}</div>
       <div className={styles.messages}>{messagesElements}</div>
+      <textarea style={{width: '300px'}}
+                ref={newMessageElement}>
+      </textarea>
+      <button style={{width: '50px', height: '30px', alignSelf: 'end'}}
+              onClick={sendMessage}
+      >Send
+      </button>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {RefObject} from 'react';
 import styles from './MyPosts.module.css';
 import Post from './Post/Post';
 import {PostsDataType} from '../../../Redux/state';
@@ -10,14 +10,21 @@ type MyPostsType = {
 const MyPosts: React.FC<MyPostsType> = (props) => {
 const postsElements = props.posts.map(p => <Post id={p.id} message={p.message} like={p.likesCount} key={p.id}/>);
 
+  let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+const addPost = () => {
+  let text = newPostElement.current?.value
+  alert(text)
+}
+
   return (
     <div className={styles.postsBlock}>My posts
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add</button>
+          <button onClick={addPost}>Add</button>
         </div>
       </div>
       <div className={styles.posts}>{postsElements}</div>
