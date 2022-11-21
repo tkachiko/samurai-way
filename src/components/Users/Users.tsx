4a4fdb9bc40bcfd6d52bbf6376d3eb4504643d24
tkachiko <tkachikos@gmail.com>
2022-11-21 23:52:53 +0300

    follow/unfollow api

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,6 +3,8 @@ import styles from './Users.module.css';
 import userPhoto from '../../assets/images/user-image.png';
 import {UserType} from '../../redux/users-reducer';
 import {NavLink} from 'react-router-dom';
+import {default as axios} from 'axios';
+import {API_KEY} from '../../secret-variables';
 
 type UsersComponentType = {
   users: UserType[]
@@ -42,8 +44,34 @@ const Users = (props: UsersComponentType) => {
           </div>
           <div>
             {u.followed
-              ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-              : <button onClick={() => props.follow(u.id)}>Follow</button>}
+              ? <button onClick={() => {
+                axios
+                  .delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                    withCredentials: true,
+                    headers: {
+                      'API-KEY': API_KEY
+                    }
+                  })
+                  .then((response: any) => {
+                    if(response.data.resultCode === 0) {
+                      props.unfollow(u.id);
+                    }
+                  });
+              }}>Unfollow</button>
+              : <button onClick={() => {
+                axios
+                  .post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                    withCredentials: true,
+                    headers: {
+                      'API-KEY': API_KEY
+                    }
+                  })
+                  .then((response: any) => {
+                    if(response.data.resultCode === 0) {
+                      props.follow(u.id);
+                    }
+                  });
+              }}>Follow</button>}
           </div>
         </span>
         <span>
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -37,7 +37,7 @@ class UsersContainer extends React.Component<UsersPropsType> {
   componentDidMount() {
     this.props.toggleIsFetching(true);
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials: true})
       .then((response: any) => {
         this.props.toggleIsFetching(false);
         this.props.setUsers(response.data.items);
@@ -49,7 +49,7 @@ class UsersContainer extends React.Component<UsersPropsType> {
     this.props.setCurrentPage(pageNumber);
     this.props.toggleIsFetching(true);
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true})
       .then((response: any) => {
         this.props.toggleIsFetching(false);
         this.props.setUsers(response.data.items);
