b67b2d880835418a0d0a99df502a83d2ef4e1591
tkachiko <tkachikos@gmail.com>
2022-12-11 00:23:09 +0300

    setState, local state. ProfileStatus

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from './ProfileInfo.module.css';
 import {Preloader} from '../../../common/Preloader/Preloader';
 import {ProfilePropsType} from '../../Profile';
+import {ProfileStatus} from './ProfileStatus';
 
 const ProfileInfo: React.FC<ProfilePropsType> = (props) => {
-  if(!props.profile) {
-    return <Preloader/>
+  if (!props.profile) {
+    return <Preloader/>;
   }
 
   return (
@@ -15,7 +16,8 @@ const ProfileInfo: React.FC<ProfilePropsType> = (props) => {
           src={'https://img.freepik.com/free-vector/night-ocean-landscape-full-moon-and-stars-shine_107791-7397.jpg?w=2000'}
           alt={'cover'}/>
         <div className={styles.descriptionBlock}>
-          <img src={props.profile.photos.large} alt={'user avatar'}/> + description
+          <img src={props.profile.photos.large} alt={'user avatar'}/>
+          <ProfileStatus status={'Hey yo!'}/>
         </div>
       </div>
     </div>
---
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileStatus.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type ProfileStatusPropsType = {
+  status: string
+}
+
+export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
+  state = {
+    editMode: false
+  };
+
+  activateEditMode = () => {
+    this.setState({editMode: true});
+  };
+
+  deactivateEditMode = () => {
+    this.setState({editMode: false});
+  };
+
+  render() {
+    return (
+      <>
+        {!this.state.editMode &&
+          <div>
+            <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+          </div>
+        }
+        {this.state.editMode &&
+          <div>
+            <input type="text"
+                   autoFocus
+                   onBlur={this.deactivateEditMode}
+                   value={this.props.status}/>
+          </div>
+        }
+      </>
+    );
+  }
+}
