26545776f40684ea98bc1cda6fe42ab304f2d721
tkachiko <tkachikos@gmail.com>
2022-12-23 23:58:43 +0300

    reselect

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,8 +1,12 @@
 import {RootStateType} from './redux-store'
+import {createSelector} from 'reselect'
 
-export const getUsers = (state: RootStateType) => {
+export const getUsersSelector = (state: RootStateType) => {
   return state.usersPage.users
 }
+export const getUsers = createSelector(getUsersSelector, (users) => {
+  return users.filter(u => true)
+})
 
 export const getPageSize = (state: RootStateType) => {
   return state.usersPage.pageSize
