b0715c2726fa6aa6cade1c2c4eaf617dc4d8f3ca
tkachiko <tkachikos@gmail.com>
2022-12-28 17:40:48 +0300

    hook useState

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styles from './ProfileInfo.module.css';
-import {Preloader} from '../../../common/Preloader/Preloader';
-import {ProfilePropsType} from '../../Profile';
-import {ProfileStatus} from './ProfileStatus';
+import React from 'react'
+import styles from './ProfileInfo.module.css'
+import {Preloader} from '../../../common/Preloader/Preloader'
+import {ProfilePropsType} from '../../Profile'
+import {ProfileStatusWithHooks} from './ProfileStatusWithHooks'
 
 const ProfileInfo: React.FC<ProfilePropsType> = (props) => {
   if (!props.profile) {
-    return <Preloader/>;
+    return <Preloader />
   }
 
   return (
@@ -14,14 +14,14 @@ const ProfileInfo: React.FC<ProfilePropsType> = (props) => {
       <div className={styles.main}>
         <img
           src={'https://img.freepik.com/free-vector/night-ocean-landscape-full-moon-and-stars-shine_107791-7397.jpg?w=2000'}
-          alt={'cover'}/>
+          alt={'cover'} />
         <div className={styles.descriptionBlock}>
-          <img src={props.profile.photos.large} alt={'user avatar'}/>
-          <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+          <img src={props.profile.photos.large} alt={'user avatar'} />
+          <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProfileInfo;
+export default ProfileInfo
---
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,42 @@
+import React, {ChangeEvent, FC, useState} from 'react'
+
+type ProfileStatusPropsType = {
+  status: string
+  updateStatus: (status: string) => void
+}
+
+export const ProfileStatusWithHooks: FC<ProfileStatusPropsType> = (props) => {
+  const [editMode, setEditMode] = useState<boolean>(false)
+  const [status, setStatus] = useState<string>(props.status)
+
+  const activateEditMode = () => {
+    setEditMode(true)
+  }
+  const deactivateEditMode = () => {
+    setEditMode(false)
+    props.updateStatus(status)
+  }
+
+  const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setStatus(e.currentTarget.value)
+  }
+
+  return (
+    <>
+      {!editMode &&
+        <div>
+          <span onDoubleClick={activateEditMode}>{props.status || '------'}</span>
+        </div>
+      }
+      {editMode &&
+        <div>
+          <input type="text"
+                 autoFocus
+                 onBlur={deactivateEditMode}
+                 onChange={onStatusChange}
+                 value={status} />
+        </div>
+      }
+    </>
+  )
+}
